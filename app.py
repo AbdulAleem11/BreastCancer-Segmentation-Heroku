e3f32cce568c6ab116bc0506db7825d08edb7e0c
AbdulAleem11 <heroliabdul@gmail.com>
2022-05-28 23:25:45 +0530

    Segmentation

--- app.py
+++ app.py
@@ -4,7 +4,8 @@
 
 
 import numpy as np
-# Keras
+import cv2
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
@@ -15,36 +16,24 @@ from flask import Flask, redirect, url_for, request, render_template,jsonify
 
 app = Flask(__name__)
 
-dic = {0 : 'COVID', 1 : 'Normal' , 2 : "Pneumonia"}
+
 
 from tensorflow.keras.models import load_model
-model = load_model('covid_model_resnet50_b32_e5_acc92.23.h5')
+model = load_model('best_weights.h5')
 
-#model.make_predict_function()
 
-def predict_label(img_path):
-    from tensorflow.keras.applications.resnet import preprocess_input 
-    img = image.load_img(img_path, target_size=(224, 224))       
-    # Preprocessing the image
-    x=image.img_to_array(img)
-    x=np.expand_dims(x,axis=0)
-    img_data=preprocess_input(x)#a=1  
-    a=np.argmax(model.predict(img_data), axis=1)
-    
-    if(a==0):
-        preds="92.23%  chances of the patient been diagnosed with COVID"
-    elif(a==1):
-        preds="92.23%  chances of the patient's lungs are detected normal"
-    else:
-        preds="92.23%  chances of the patient been diagnosed with Viral Pneumonia"
-    return preds
-    
-	# i = image.load_img(img_path, target_size=(100,100))
-	# i = image.img_to_array(i)/255.0
-	# i = i.reshape(1, 100,100,3)
-	# p = model.predict_classes(i)
-	# return dic[p[0]]
 
+
+def predictBreastSegment(imgPath, model, shape = 256):
+    img = plt.imread(imgPath)
+    img = cv2.resize(img, (shape, shape)) 
+    predictions = model.predict(np.array([img]))
+    return predictions, img
+
+def Plotter(predMask):
+    predMask=predMask.squeeze()
+    plt.imsave("static/mask.png", predMask, format="png",  cmap='cividis')
+    return "static/mask.png"
 
 # routes
 @app.route("/", methods=['GET', 'POST'])
@@ -62,10 +51,10 @@ def get_output():
 
 		img_path = "static/" + img.filename	
 		img.save(img_path)
+		Prediction, actuals = predictBreastSegment(img_path, model)
+		mask_path=Plotter(Prediction[0])
 
-		p = predict_label(img_path)
-
-	return render_template("index.html", prediction = p, img_path = img_path)
+	return render_template("index.html",img_path = img_path,mask_path=mask_path)
 
 
 if __name__ =='__main__':
